db1eaf935fa02105da2ea71a617153e4e25bfe48
corneliusschramm <cornelius.schramm@finn.auto>
2024-03-17 11:07:27 -0400

    Add expo-av dependency and attempt to implement audio recording feature

--- src/app/recording.tsx
+++ src/app/recording.tsx
@@ -1,31 +1,42 @@
-import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from 'react';
+import { View, Text,StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
-export default function Page() {
+export default function App() {
+  const [sound, setSound] = useState<Audio.Sound | undefined>();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound:loadedSound } = await Audio.Sound.createAsync( require('@public/Hello.mp3')
+    );
+    setSound(loadedSound);
+    
+    console.log('Playing Sound');
+    await loadedSound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound ? () => {
+      console.log('Unloading Sound');
+      sound.unloadAsync();
+    } : undefined;
+  }, [sound]);
+
   return (
-    <View className="flex flex-1 bg-white dark:bg-black">
-      <Content />
+    <View style={styles.container}>
+      <Text className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-5xl p-7 text-black dark:text-white" >Record Audio Here 🎤</Text>
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
 }
 
-function Content() {
-  return (
-    <View className="flex-1 justify">
-          <View className="flex flex-col items-center gap-2 pt-10">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-5xl p-7 text-black dark:text-white"
-            >
-              Record Audio Here 🎤
-            </Text>
-            <View>
-              <Text className="text-white bg-green-200">
-                Test
-              </Text>
-            </View>
-          </View>
-    </View>
-  );
-}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+  },
+});
