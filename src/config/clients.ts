bb687bc7a696eef24718a2827b2212b0bb39d38d
corneliusschramm <cornelius.schramm@finn.auto>
2024-04-25 20:47:38 -0400

    install telnxy client

--- src/config/clients.ts
+++ src/config/clients.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 const OPEN_API_KEY = process.env.OPEN_API_KEY;
 export const openai = new OpenAI({ apiKey: OPEN_API_KEY });
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
