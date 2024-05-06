b132fe24a732d4de32280effec0599fed50668fe
CorneliusSchramm <44907362+CorneliusSchramm@users.noreply.github.com>
2024-05-06 15:57:58 -0400

    Add getCurrentDateTimeUrlEncoded function to datetime.ts

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -3,3 +3,8 @@ import { DateTime } from "luxon";
 export function getCurrentTime() {
   return DateTime.now().toFormat("yyyy-MM-dd HH:mm:ss");
 }
+
+export function getCurrentDateTimeUrlEncoded(): string {
+  const now = DateTime.now().toISO();
+  return encodeURIComponent(now);
+}
