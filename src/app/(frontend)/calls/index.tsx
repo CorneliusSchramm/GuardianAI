7035b2fd1d792f0d09da41c4fb410970562f7a83
CorneliusSchramm <44907362+CorneliusSchramm@users.noreply.github.com>
2024-05-09 19:23:48 -0400

    chore: Update calls page to fetch and display call data from supabase (list working)

--- src/app/(frontend)/calls/index.tsx
+++ src/app/(frontend)/calls/index.tsx
@@ -1,41 +1,127 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet , TouchableOpacity} from "react-native";
-import { getCallOverview } from '@/backend/data/callRepository';
-export default function Page() {
-  const [callDetails, setCallDetails] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const callId = 32
-  // this code now produces an error with missing OPENAI_API_KEY
-  // I think it may be related to: 
-//   The following packages should be updated for best compatibility with the installed expo version:
-//   expo@50.0.14 - expected version: ~50.0.17
-//   expo-av@13.10.5 - expected version: ~13.10.6
-//   expo-constants@15.4.5 - expected version: ~15.4.6
-//   expo-router@3.4.8 - expected version: ~3.4.10
-//   expo-splash-screen@0.26.4 - expected version: ~0.26.5
-// Your project may not work correctly until you install the correct versions of the packages.
+import React, {Component, useEffect, useState} from 'react';
+import {StyleSheet, Alert, FlatList, ActivityIndicator} from 'react-native';
+import {Colors, BorderRadiuses, View, Image, ListItem, Text } from 'react-native-ui-lib';
+import orders, {OrderType} from '@assets/orders';
+import { supabase } from '@/frontend/lib/supabase';
 
-//   useEffect(() => {
-//     async function fetchCallDetails() {
-//         try {
-//             const data = await getCallOverview(callId);
-//             setCallDetails(data);
-//             setError(null);
-//         } catch (err) {
-//             setError((err as Error).message);
-//             setCallDetails(null);
-//         } finally {
-//             setLoading(false);
-//         }
-//     }
 
-//     if (!isNaN(callId)) {
-//         fetchCallDetails();
-//     } else {
-//         setError('Invalid Call ID');
-//         setLoading(false);
-//     }
-// }, [callId]);
-  return <Text className='h-7'>Home page </Text>;
+type CallData = {
+  call_id: number;
+  call_start_datetime: string;
+  transcription: string;
+  score: number;
+  category: string;
 }
+
+export default function App () {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<CallData[]>([]);
+
+  const getMovies = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("v_calls_with_analyses")
+        .select("call_id, call_start_datetime, transcription,score,category")
+        .not("score", "is", null)
+      
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <View style={{flex: 1, padding: 24}}>
+      <Text text30BO center>Call Data</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        
+        <FlatList
+            data={data}
+            keyExtractor={({ call_id }) => call_id.toString()}
+            renderItem={({ item }) => (
+              <Text>
+                {item.call_id}, {item.category}
+              </Text>
+            )} />
+      )}
+    </View>
+  );
+};
+
+
+// export default class BasicListScreen extends Component {
+
+//   keyExtractor = (item: OrderType) => item.name;
+
+//   renderRow(row: OrderType, id: number) {
+//     const statusColor = row.inventory.status === 'Paid' ? Colors.green30 : Colors.red30;
+
+//     return (
+//       <View>
+//         <ListItem
+//           activeBackgroundColor={Colors.grey60}
+//           activeOpacity={0.3}
+//           height={77.5}
+//           onPress={() => Alert.alert(`pressed on order #${id + 1}`)}
+//         >
+//           <ListItem.Part left>
+//             <Image source={{uri: row.mediaUrl}} style={styles.image}/>
+//           </ListItem.Part>
+//           <ListItem.Part middle column containerStyle={[styles.border, {paddingRight: 17}]}>
+//             <ListItem.Part containerStyle={{marginBottom: 3}}>
+//               <Text grey10 text70 style={{flex: 1, marginRight: 10}} numberOfLines={1}>
+//                 {row.name}
+//               </Text>
+//               <Text grey10 text70 style={{marginTop: 2}}>
+//                 {row.formattedPrice}
+//               </Text>
+//             </ListItem.Part>
+//             <ListItem.Part>
+//               <Text
+//                 style={{flex: 1, marginRight: 10}}
+//                 text90
+//                 grey40
+//                 numberOfLines={1}
+//               >{`${row.inventory.quantity} item`}</Text>
+//               <Text text90 color={statusColor} numberOfLines={1}>
+//                 {row.inventory.status}
+//               </Text>
+//             </ListItem.Part>
+//           </ListItem.Part>
+//         </ListItem>
+//       </View>
+//     );
+//   }
+
+//   render() {
+//     return (
+      
+//       <FlatList
+//         data={orders}
+//         renderItem={({item, index}) => this.renderRow(item, index)}
+//         keyExtractor={this.keyExtractor}
+//       />
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   image: {
+//     width: 54,
+//     height: 54,
+//     borderRadius: BorderRadiuses.br20,
+//     marginHorizontal: 14
+//   },
+//   border: {
+//     borderBottomWidth: StyleSheet.hairlineWidth,
+//     borderColor: Colors.grey70
+//   }
+// });
