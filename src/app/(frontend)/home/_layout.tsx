683a919c775ae06fa96c6f519bac141ca64aaebf
Moritz Bartusch <moritz.bartusch@finn.com>
2024-05-09 21:35:46 -0400

    Add Tabs component to Layout

---
+++ src/app/(frontend)/home/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../../global.css";
+import { Slot, Stack, Tabs } from "expo-router";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Auth from "@/frontend/components/Auth";
+
+
+export default function Layout() {
+  return <Tabs/>;
+}
+
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "../global.css";
-import { Slot, Stack } from "expo-router";
+import { Slot, Stack, Tabs } from "expo-router";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Auth from "@/frontend/components/Auth";
+import Home from "./(frontend)/home/home";
+import Settings from "./(frontend)/home/settings";
+
 
 export default function Layout() {
-  return <Stack />;
+  return <Stack
+    screenOptions={{
+      headerShown: false,
+      headerBackTitleVisible: true,
+      // headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }}/>;
 }
