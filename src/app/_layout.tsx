05834e3db5dea07187806cfd239478df22721ca7
CorneliusSchramm <44907362+CorneliusSchramm@users.noreply.github.com>
2024-05-10 17:51:20 -0400

    fix paths

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../global.css";
 import { Slot, Stack } from "expo-router";
-import QueryProvider from "@/frontend/providers/src/providers/QueryProvider";
+import QueryProvider from "@/frontend/providers/QueryProvider";
 
 export default function Layout() {
   
