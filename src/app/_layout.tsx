b7b74c3912d3ef76790aa9565d63ffb3eaf35191
CorneliusSchramm <44907362+CorneliusSchramm@users.noreply.github.com>
2024-05-10 16:39:24 -0400

    chore: Add QueryProvider for improved data fetching

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "../global.css";
 import { Slot, Stack } from "expo-router";
+import QueryProvider from "@/frontend/providers/src/providers/QueryProvider";
 
 export default function Layout() {
-  return <Stack />;
+  
+  return (
+    <QueryProvider>
+      <Stack />
+    </QueryProvider>
+  )
 }
