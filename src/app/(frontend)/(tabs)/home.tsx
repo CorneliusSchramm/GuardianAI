9866f97b8a363db20f9cc48e7ee035b754eb5e3b
CorneliusSchramm <44907362+CorneliusSchramm@users.noreply.github.com>
2024-05-11 18:59:14 -0400

    feat: Add PushPage component for Expo push notifications (not working - cannot find dev server

--- src/app/(frontend)/(tabs)/home.tsx
+++ src/app/(frontend)/(tabs)/home.tsx
@@ -4,6 +4,7 @@ import { Text, View, StyleSheet} from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Colors,  } from 'react-native-ui-lib';
 import { globalStyles } from '../theme';
+import PushPage from '@/frontend/components/Push';
 
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
     <View style={globalStyles.container}>
 
       <Text>Home is where the domw is</Text>
-
+      {/* <PushPage/> */}
     </View>
   );
 }
